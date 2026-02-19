65a6fd2b6397277e7e076f5dd7cb010802016319
Ayrton Carvalho <cayrton021@gmail.com>
2026-02-19 20:18:58 -0300

    Consulta simples com query methods

--- src/main/java/com/ayrtonCarvalho/workshopMongo/resource/PostResource.java
+++ src/main/java/com/ayrtonCarvalho/workshopMongo/resource/PostResource.java
@@ -3,6 +3,7 @@ package com.ayrtonCarvalho.workshopMongo.resource;
 import com.ayrtonCarvalho.workshopMongo.DoMain.Post;
 import com.ayrtonCarvalho.workshopMongo.DoMain.User;
 import com.ayrtonCarvalho.workshopMongo.dto.UserDto;
+import com.ayrtonCarvalho.workshopMongo.resource.util.URL;
 import com.ayrtonCarvalho.workshopMongo.service.PostService;
 import com.ayrtonCarvalho.workshopMongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.awt.SystemColor.text;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -25,5 +28,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = us.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity <List<Post>> findByTitle(@RequestParam (value = "text", defaultValue = "") String text) {
+
+        text = URL.decodeParam(text);
+        List<Post> list = us.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+
     }
 }
